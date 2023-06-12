5dd4750fd12544762019b355ac60cea65d1aad70
ltzLucas <ltzlucasteixeiralima@gmail.com>
2023-06-12 15:00:16 -0300

    Adicionando class MeuSeletor

--- mainSeletor.py
+++ mainSeletor.py
@@ -38,6 +38,16 @@ class Validador(db.Model):
     transacoes = db.Column(db.Integer, unique=False, nullable=False)
     aux_flag = db.Column(db.Integer, unique=False, nullable=False)
 
+@dataclass
+class MeuSeletor(db.Model):
+    id: int
+    fCoins: int
+    qtd_transacoes: int
+
+    id = db.Column(db.Integer, primary_key=True)
+    fCoins = db.Column(db.Integer, unique=False, nullable=False)
+    qtd_transacoes = db.Column(db.Integer, unique=False, nullable=False)
+
 
 @app.route("/")
 def index():
